b912dcc3048e049ee43736834c82d2dcabff6c3f
yuanyi <603035634@qq.com>
2017-07-15 17:27:30 +0800

    1

--- test1/main2.cpp
+++ test1/main2.cpp
@@ -42,22 +42,55 @@ void func(int * arr, int len) {
 	int begin = 0;
 	int tail = len - 1;
 	while (begin < tail) {
-		while (arr[begin] % 2 == 0 && begin < tail) {
+		while (arr[begin] % 2 != 0 && begin <= tail) {
 			begin++;
 		}
-		while (arr[tail--] % 2 != 0 && tail >= 0) {
+		while (arr[tail] % 2 == 0 && tail >= 0) {
 			tail--;
 		}
+		cout << begin << ' ' << tail << endl;
 		int temp = arr[begin];
 		arr[begin] = arr[tail];
 		arr[tail] = temp;
+		begin++;
+		tail--;
 	}
 	show(arr, len);
 }
 
-int main(void) {
-	int arr[] = {4,11,16,20,40,15,13,2,3,1,8,4};
-	func(arr, 12);
+void m_sort(int *arr, int begin, int end) {
+	if (begin >= end) {
+		return;
+	}
+	
+	int cmp = arr[begin];
+	int i = begin;
+	int d = end;
+	while (i < d) {
+		while ( i < d && arr[i] < cmp) {
+			++i;
+		}
+		while ( i < d && arr[d] >= cmp) {
+			--d;
+		}
 
+		if (i < d) {
+			int temp = arr[i];
+			arr[i] = arr[d];
+			arr[d] = temp;
+		}
+	}
+
+	m_sort(arr, begin, i);
+	m_sort(arr, i + 1 , end);
+
+	return;
+}
+
+int main(void) {
+	int arr[] = {4,11,16,20,40,15,13,2,3,1,8,4,13};
+	show(arr, 13);
+	m_sort(arr, 0, 12);
+	show(arr, 13);
 	return 0;
 }
